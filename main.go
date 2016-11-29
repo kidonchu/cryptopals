18d4a6adbdc47b4ebad05ff09ddf0ef728736590
Kidon Chu <kidonchu@gmail.com>
2016-11-28 18:16:12 -0600

    Add hexFixedXOR

--- main.go
+++ main.go
@@ -8,16 +8,19 @@ import (
 )
 
 func main() {
-	msg := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-	dst, err := HexToBase64(msg)
+
+	src1 := "1c0111001f010100061a024b53535009181c"
+	src2 := "686974207468652062756c6c277320657965"
+
+	dst, err := hexFixedXOR(src1, src2)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	fmt.Printf("dst = %+v\n", dst)
 }
 
-// HexToBase64 converts hexadecimal encoding to base64 encoding
-func HexToBase64(src string) (string, error) {
+func hexToBase64(src string) (string, error) {
 	decoded, err := hex.DecodeString(src)
 	if err != nil {
 		return "", err
@@ -26,5 +29,25 @@ func HexToBase64(src string) (string, error) {
 	if err != nil {
 		return "", err
 	}
+	return encoded, nil
+}
+
+func hexFixedXOR(src1 string, src2 string) (string, error) {
+	decoded1, err := hex.DecodeString(src1)
+	if err != nil {
+		return "", err
+	}
+	decoded2, err := hex.DecodeString(src2)
+	if err != nil {
+		return "", err
+	}
+
+	res := make([]byte, len(decoded1))
+	for i := 0; i < len(decoded1); i++ {
+		res[i] = decoded1[i] ^ decoded2[i]
+	}
+
+	encoded := hex.EncodeToString(res)
+
 	return encoded, nil
 }
